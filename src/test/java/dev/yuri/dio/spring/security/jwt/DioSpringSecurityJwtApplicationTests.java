0213218244b04272e9ac4cc53f0454bb4cb7e025
yuri-italo <yuri.italo94@gmail.com>
2023-05-26 11:07:18 -0300

    configuracao do projeto

---
+++ src/test/java/dev/yuri/dio/spring/security/jwt/DioSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yuri.dio.spring.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
